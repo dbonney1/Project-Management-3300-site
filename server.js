84f45af101a64010a2bbe2b887d65dd6e0048ba2
Devin Bonney <faline124@yahoo.com>
2020-12-19 10:37:10 -0500

    added express routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('./src'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'src', 'index.html'));
+    res.sendFile('index.html');
 });
 
 const port = process.env.PORT || 5001;
